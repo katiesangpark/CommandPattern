54c0e9ccd47c93e1a42a77a89c9f1fa93c82afc1
Katie Park <katie.sang.park@gmail.com>
2017-11-09 22:15:11 -0800

    init

---
+++ CommandPatternTutorial/Receiver/AirConditioner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPatternTutorial.Receiver
+{
+    public class AirConditioner
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Air Conditioner is off!");
+
+        }
+    }
+}
---
+++ CommandPatternTutorial/Receiver/Light.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPatternTutorial.Receiver
+{
+    //turn on and off
+    public class Light
+    {
+        public void On()
+        {
+            Console.WriteLine("Lights are on!");
+        }
+        public void off()
+        {
+            Console.WriteLine("Lights are off!");
+        }
+    }
+}
