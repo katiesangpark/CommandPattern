2fdf284290ece2afad2f7e230606711e982e64b6
Katie Park <katie.sang.park@gmail.com>
2017-11-09 22:40:37 -0800

    AirConditioner On and Off

---
+++ CommandPatternTutorial/ConcreteCommand/AirConditionerOff.cs
@@ -0,0 +1,27 @@
+﻿using CommandPatternTutorial.AbstractCommand;
+using CommandPatternTutorial.Receiver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPatternTutorial.ConcreteCommand
+{
+    public class AirConditionerOff : ICommand
+    {
+        AirConditioner ac;
+
+        public AirConditionerOff(AirConditioner ac)
+        {
+            this.ac = ac;
+        }
+        public void Execute()
+        {
+            ac.TurnOff();
+            ac.DecreaseTemp();
+        }
+  
+    
+    }
+}
---
+++ CommandPatternTutorial/ConcreteCommand/AirConditionerOn.cs
@@ -0,0 +1,26 @@
+﻿using CommandPatternTutorial.AbstractCommand;
+using CommandPatternTutorial.Receiver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CommandPatternTutorial.ConcreteCommand
+{
+    public class AirConditionerOn: ICommand
+    {
+        AirConditioner ac;
+
+        public AirConditionerOn(AirConditioner ac)
+        {
+            this.ac = ac;
+        }
+        public void Execute()
+        {
+            ac.TurnOn();
+            ac.IncreaseTemp();
+        }
+    }
+}
