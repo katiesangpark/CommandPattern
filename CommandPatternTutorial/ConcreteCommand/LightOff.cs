2f4a92a89fbf4eddf9f64715d81f0463f1e20de2
Katie Park <katie.sang.park@gmail.com>
2017-11-09 22:30:13 -0800

    ConcreteCommand - Light on and off

---
+++ CommandPatternTutorial/ConcreteCommand/LightOff.cs
@@ -0,0 +1,24 @@
+﻿using CommandPatternTutorial.AbstractCommand;
+using CommandPatternTutorial.Receiver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPatternTutorial.ConcreteCommand
+{
+    public class LightOff : ICommand
+    {
+        Light light;
+
+        public LightOff(Light light)
+        {
+            this.light = light;
+        }
+        public void Execute()
+        {
+            light.off();
+        }
+    }
+}
---
+++ CommandPatternTutorial/ConcreteCommand/LightOn.cs
@@ -0,0 +1,25 @@
+﻿using CommandPatternTutorial.AbstractCommand;
+using CommandPatternTutorial.Receiver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPatternTutorial.ConcreteCommand
+{
+    public class LightOn : ICommand
+    {
+        Light light;
+        //constructor
+        public LightOn(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute()
+        {
+            light.On();
+        }
+    }
+}
